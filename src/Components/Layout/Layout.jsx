bedf0d59ce077d96df29b45a429827bb8a7adbd7
Adham-aliii <168802275+Adham-aliii@users.noreply.github.com>
2024-08-31 22:53:32 +0300

    Done

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useState } from 'react'
+import style from './Layout.module.css'
+import Navbar from '../Navbar/Navbar.jsx'
+import Footer from '../Footer/Footer.jsx'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { UserContext } from '../../Context/UserContext.jsx'
+
+export default function Layout() {
+
+
+  // let { setUserData } = useContext(UserContext)
+  // let navigate = useNavigate()
+
+  // useEffect(() => {
+  //   if (localStorage.getItem('userToken')) {
+  //     setUserData(localStorage.getItem('userToken'))
+  //   } else {
+  //     navigate('/login')
+  //   }
+  // }, [])
+
+
+
+  return <>
+    <Navbar />
+    <div className="md:pt-12">
+
+      <Outlet></Outlet>
+    </div>
+    
+    <Footer />
+  </>
+}
